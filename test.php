eb5b235a0477aba10dd8b76a4106ca95d14fbb86
LabidiAlaa <labidi.alaa.enicarthage@gmail.com>
2017-07-13 12:15:04 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,42 +1,48 @@
 <?php
-$servername = "iris-tech-azure-app-mysqldbserver.mysql.database.azure.com";
-$username = "mysqldbuser@iris-tech-azure-app-mysqldbserver";
-$password = "dev@tech2";
-$dbname = "mysqldatabase49658";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-    echo "connection failed";
+// Connexion à la base de données
+try
+{
+	$bdd = new PDO('mysql:host=iris-tech-azure-app-mysqldbserver.mysql.database.azure.com;dbname=mysqldatabase49658;charset=utf8', 'mysqldbuser@iris-tech-azure-app-mysqldbserver', 'dev@tech2');
 }
-$ruche_id = $_GET['ruche_id'];
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+//echo $_POST['title'];
+if ('1'=='1'){
+	$id_ruche = $_GET['id_ruche'];
+	$temperature1 = $_GET['temperature1'];
+	$temperature2 = $_GET['temperature2'];
+	$humidite = $_GET['humidite'];
+	$count=0 ;
+	$cap_bat = $_GET['cap_bat'];
+	$lati = $_GET['lati'];
+	$longi = $_GET['longi'];
+	
+	
+	
+	
+		
+	$sql = "INSERT INTO data( id_ruche, temperature1, temperature2, humidite, count, cap_bat, lati, longi ) values ('ALaa_test_hive_50', 0, 0, 0, 0, 0, 0, 0)";
+	//$req = $bdd->prepare($sql);
+	//$req->execute();
+	
+	echo $sql;
+	
+	$query = $bdd->prepare( $sql );
+	if ($query == false) {
+	 print_r($bdd->errorInfo());
+	 die ('Erreur prepare');
+	}
+	$sth = $query->execute();
+	if ($sth == false) {
+	 print_r($query->errorInfo());
+	 die ('Erreur execute');
+	}
+
+}
 
 
-$sql = "INSERT INTO `mysqldatabase49658`.`data`
-(`ruche_id`,
-`temperature1`,
-`temperature2`,
-`humidite1`,
-`count`,
-`cap_bat`,
-`lati`,
-`longi`)
-VALUES
-(<{ruche_id: }>,
-<{temperature1: 0}>,
-<{temperature2: 0}>,
-<{humidite1: 0}>,
-<{count: 0}>,
-<{cap_bat: 0}>,
-<{lati: 0}>,
-<{longi: 0}>);";
-
-
-
-
-echo "welcome to azure, ".$ruche_id."";
-
-
+	
 ?>
