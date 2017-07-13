5b91c8c738531713f2d611012588cb04224e0860
LabidiAlaa <labidi.alaa.enicarthage@gmail.com>
2017-07-13 11:38:13 +0100

    Update test.php

--- test.php
+++ test.php
@@ -10,9 +10,21 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$ism = $_GET['id'];
+$ruche_id = $_GET['ruche_id'];
+$temperature1 = $_GET['temperature1'];
+$temperature2 = $_GET['temperature2'];
+$humidite1 = $_GET['humidite1'];
+$count = $_GET['count'];
+$cap_bat = $_GET['cap_bat'];
+$lati = $_GET['lati'];
+$longi = $_GET['longi'];
 
-echo "welcome to azure ".$ism."";
+$sql = "INSERT INTO data(ruche_id, temperature1, temperature2, humidite1, count, cap_bat, lati, longi) values ('$ruche_id', '$temperature1', '$temperature2', '$humidite1', '$count', '$cap_bat', '$lati', '$longi')";
+
+
+
+
+echo "welcome to azure, data successfully stored";
 
 
 ?>
